452523cde8e0179add46fab2c78ea3d31b445fd9
M4thG33k <pepethemathgeek@gmail.com>
2015-05-28 11:48:55 -0500

    Bare-bones mod outline. Nothing too interesting here.

---
+++ src/main/java/com/M4thG33k/m4ththings/proxy/CommonProxy.java
@@ -0,0 +1,13 @@
+package com.M4thG33k.m4ththings.proxy;
+
+/**
+ * Created by M4thG33k on 5/28/2015.
+ */
+public class CommonProxy implements IProxy{
+
+    public void registerRenderers()
+    {
+
+    }
+
+}
